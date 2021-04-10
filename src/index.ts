0e391c953450fbdb1f1480829b0de8358203ee5d
Jonathan <jtellkampf@gmail.com>
2021-04-10 17:06:45 +0100

    Added on and trigger methods to user model to create eventing system

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "Sam", age: 20 });
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.set({ name: "John", age: 40 });
+
+user.on("change", () => {
+  console.log("Change 1");
+});
+
+user.on("change", () => {
+  console.log("Change 2");
+});
+
+user.on("change", () => {
+  console.log("Change 3");
+});
+
+user.trigger("change");
