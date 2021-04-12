dd619323894574ca0ccc2b9cec8a24ef28550681
Jonathan <jtellkampf@gmail.com>
2021-04-12 21:28:51 +0100

    Added fetch and save method to user class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "new record", age: 90 });
+const user = new User({ id: 1, name: "newer name", age: 55 });
+
+user.on("save", () => {
+  console.log(user);
+});
+
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
@@ -13,10 +14,10 @@ const rootUrl = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attibutes: Attributes<UserProps>;
+  public attributes: Attributes<UserProps>;
 
   constructor(attrs: UserProps) {
-    this.attibutes = new Attributes<UserProps>(attrs);
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 
   get on() {
@@ -28,6 +29,34 @@ export class User {
   }
 
   get get() {
-    return this.attibutes.get;
+    return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger("save");
+      })
+      .catch(() => {
+        this.trigger("error");
+      });
   }
 }
